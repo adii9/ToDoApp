a9a37099cbc1a659c1ff197baad71d53c436ac90
Aditya Mathur (TTL) <am925941.ttl@tatamotors.com>
2023-02-09 11:16:14 +0530

    Added Cards to View

--- frontend/src/components/Display.js
+++ frontend/src/components/Display.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { useEffect, useState } from 'react'
 import API from '../API';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import { useNavigate } from 'react-router-dom';
 
 function Display() {
     const [APIData, setAPIData] = useState([])
@@ -8,6 +11,8 @@ function Display() {
     const [description, setDescription] = useState('')
     const [status, setStatus] = useState('')
     const [message, setMessage] = useState('')
+
+    const navigate = useNavigate()
 
     useEffect(() => {
         refreshData()
@@ -21,30 +26,37 @@ function Display() {
             .catch(console.error)
     }
 
+    const editTask = (index) => {
+
+        navigate('/update', {state: {index: index}})
+    }
+
+
     return (
         <>
 
             <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
                 <h1 style={{ color: '#7550C7' }}>To Do List</h1>
             </div>
-            <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+            <div style={{ display: 'flex', justifyContent: 'space-evenly' }}>
                 {APIData.length != 0 ?
-                    <table style={{ width: '70%', border: '1px', borderColor: 'black', borderStyle: 'double' }}>
-                        <tr>
-                            <th>Id</th>
-                            <th>Title</th>
-                            <th>Description</th>
-                            <th>Status</th>
-                        </tr>
-                        {APIData.map((data) => (
-                            <tr key={data.id}>
-                                <td>{data.id}</td>
-                                <td>{data.title}</td>
-                                <td>{data.description}</td>
-                                <td>{data.status}</td>
-                            </tr>
+                    <div>
+                        {APIData.flatMap((item, index) => (
+                            <Card key={index} style={{ width: '18rem', margin: 20}}>
+                                <Card.Header>
+                                    {item.status}
+                                </Card.Header>
+                                <Card.Title>
+                                    {item.title}
+                                </Card.Title>
+                                <Card.Text>
+                                    {item.description}
+                                </Card.Text>
+                                <Button variant="primary" onClick={() => editTask(item.id)}>Edit Task</Button>
+                            </Card>
                         ))}
-                    </table> :
+                    </div>
+                    :
                     <p>No Data To Display</p>
                 }
             </div>
@@ -55,3 +67,21 @@ function Display() {
 }
 
 export default Display;
+
+
+// <table style={{ width: '70%', border: '1px', borderColor: 'black', borderStyle: 'double' }}>
+                    //     <tr>
+                    //         <th>Id</th>
+                    //         <th>Title</th>
+                    //         <th>Description</th>
+                    //         <th>Status</th>
+                    //     </tr>
+                    //     {APIData.map((data) => (
+                    //         <tr key={data.id}>
+                    //             <td>{data.id}</td>
+                    //             <td>{data.title}</td>
+                    //             <td>{data.description}</td>
+                    //             <td>{data.status}</td>
+                    //         </tr>
+                    //     ))}
+                    // </table>
--- frontend/src/components/Update.js
+++ frontend/src/components/Update.js
@@ -1,11 +1,99 @@
 
 import React from 'react';
+import { useEffect, useState } from 'react'
+import API from '../API';
+import { useNavigate, useLocation } from 'react-router-dom';
 
-function Update (){
+
+function Update() {
+    const [APIData, setAPIData] = useState([])
+    const [title, setTitle] = useState('')
+    const [description, setDescription] = useState('')
+    const [status, setStatus] = useState('')
+    const {state} = useLocation();
+    const {index} = state
+
+    console.log("State Value from Route is -> ", index)
+
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        refreshData()
+    }, [])
+
+    const refreshData = () => {
+        API.get('/')
+            .then((res) => {
+                setAPIData(res.data)
+            })
+            .catch(console.error)
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+        addToDo(title, description, status)
+    }
+
+    const addToDo = (title, description, status) => {
+        let item = { title, description, status }
+        API.post('/', item)
+            .then(() => {
+                setTitle('')
+                setDescription('')
+                setStatus('')
+                refreshData()
+                alert("Data Pushed Successfully")
+                navigate('/', { replace: true });
+            })
+    }
+
+    const fetchData = (id) => {
+        API.get('/' + id)
+            .then((res) => {
+                console.log("Result for Specific Data -> ", res.data)
+            })
+    }
+
+    fetchData(index)
+
     return (
         <>
             <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
                 <h1 style={{ color: '#7550C7' }}>To Do List</h1>
+            </div>
+
+            <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                <form onSubmit={onSubmit}>
+                    <label>Task Title: </label>
+                    <input
+                        type='text'
+                        required
+                        placeholder='Title of Your Task'
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                    >
+                    </input>
+
+                    <label>Description of the Task : </label>
+                    <input
+                        type='text'
+                        required
+                        placeholder='Describe your Task'
+                        value={description}
+                        onChange={(e) => setDescription(e.target.value)}
+                    >
+                    </input>
+
+                    <label>Status: </label>
+                    <select required onChange={(e) => setStatus(e.target.value)} value={status}>
+                        <option defaultValue={true}>Select one from the List</option>
+                        <option value={"Completed"}>Completed</option>
+                        <option value={"Not Completed"}>Not Completed</option>
+                        <option value={"In Progress"}>In Progress</option>
+                    </select>
+
+                    <input type='submit' />
+                </form>
             </div>
         </>
     )
